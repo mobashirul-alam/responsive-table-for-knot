fb2d052ad57e278c2ab374fb4d14dd3f51717ae6
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-08-06 22:29:32 +0600

    row updated

--- src/Components/TableOne.js
+++ src/Components/TableOne.js
@@ -22,41 +22,6 @@ const TableOne = () => {
                         <td className="py-3 px-6 whitespace-nowrap">Blue</td>
                         <td className="py-3 px-6 whitespace-nowrap">This is fifth column</td>
                     </tr>
-                    <tr className='hover:shadow-lg hover:bg-cyan-100 hover:scale-105 duration-500 cursor-pointer border-b border-cyan-100'>
-                        <td className="py-3 px-6 whitespace-nowrap">2</td>
-                        <td className="py-3 px-6 whitespace-nowrap">Hart Hagerty</td>
-                        <td className="py-3 px-6 whitespace-nowrap">Desktop Support Technician</td>
-                        <td className="py-3 px-6 whitespace-nowrap">Purple</td>
-                        <td className="py-3 px-6 whitespace-nowrap">This is fifth column</td>
-                    </tr>
-                    <tr className='hover:shadow-lg hover:bg-cyan-100 hover:scale-105 duration-500 cursor-pointer border-b border-cyan-100'>
-                        <td className="py-3 px-6 whitespace-nowrap">3</td>
-                        <td className="py-3 px-6 whitespace-nowrap">Brice Swyre</td>
-                        <td className="py-3 px-6 whitespace-nowrap">Tax Accountant</td>
-                        <td className="py-3 px-6 whitespace-nowrap">Red</td>
-                        <td className="py-3 px-6 whitespace-nowrap">This is fifth column</td>
-                    </tr>
-                    <tr className='hover:shadow-lg hover:bg-cyan-100 hover:scale-105 duration-500 cursor-pointer border-b border-cyan-100'>
-                        <td className="py-3 px-6 whitespace-nowrap">1</td>
-                        <td className="py-3 px-6 whitespace-nowrap">Cy Ganderton</td>
-                        <td className="py-3 px-6 whitespace-nowrap">Quality Control Specialist</td>
-                        <td className="py-3 px-6 whitespace-nowrap">Blue</td>
-                        <td className="py-3 px-6 whitespace-nowrap">This is fifth column</td>
-                    </tr>
-                    <tr className='hover:shadow-lg hover:bg-cyan-100 hover:scale-105 duration-500 cursor-pointer border-b border-cyan-100'>
-                        <td className="py-3 px-6 whitespace-nowrap">2</td>
-                        <td className="py-3 px-6 whitespace-nowrap">Hart Hagerty</td>
-                        <td className="py-3 px-6 whitespace-nowrap">Desktop Support Technician</td>
-                        <td className="py-3 px-6 whitespace-nowrap">Purple</td>
-                        <td className="py-3 px-6 whitespace-nowrap">This is fifth column</td>
-                    </tr>
-                    <tr className='hover:shadow-lg hover:bg-cyan-100 hover:scale-105 duration-500 cursor-pointer border-b border-cyan-100'>
-                        <td className="py-3 px-6 whitespace-nowrap">3</td>
-                        <td className="py-3 px-6 whitespace-nowrap">Brice Swyre</td>
-                        <td className="py-3 px-6 whitespace-nowrap">Tax Accountant</td>
-                        <td className="py-3 px-6 whitespace-nowrap">Red</td>
-                        <td className="py-3 px-6 whitespace-nowrap">This is fifth column</td>
-                    </tr>
                 </tbody>
             </table>
         </div>
